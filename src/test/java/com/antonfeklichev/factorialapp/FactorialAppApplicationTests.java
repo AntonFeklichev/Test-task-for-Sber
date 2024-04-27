df1b893fc67cad6b6aaa6f9d41ba6c6e153d3fe2
Anton <afeklichev@yandex.ru>
2024-04-27 18:26:25 +0300

    init commit

---
+++ src/test/java/com/antonfeklichev/factorialapp/FactorialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonfeklichev.factorialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
